c2d8771550db4bb193fe78cce1b5067b8ff7653a
Weylland <38096642+Weylland@users.noreply.github.com>
2022-02-16 12:27:11 +0100

    Secteurs.js
    J'ai séparer le js de la page secteurs dans un fichier à part

--- js/index.js
+++ js/index.js
@@ -16,17 +16,6 @@ let slide = new Array(
   "asset/img/carrousel-3-stark.png"
 );
 let numero = 0;
-// SECTEURS 
-const btnContainer = document.getElementById("myBtnContainer");
-const allBtn = document.getElementById("allBtn");
-const lilleBtn = document.getElementById("lilleBtn");
-const arrasBtn = document.getElementById("arrasBtn");
-const touquetBtn = document.getElementById("touquetBtn");
-const btns = btnContainer.getElementsByClassName("btn");
-const x = document.getElementsByClassName("filterDiv");
-const current = document.getElementsByClassName("active");
-const show = "show";
-let clickedBtn = "all";
 
 function ChangeSlide(sens) {
   numero = numero + sens;
@@ -108,51 +97,3 @@ function flipCard() {
   this.classList.toggle("flip");
 }
 cards.forEach((Square) => Square.addEventListener("click", flipCard));
-
-// SECTEURS
-
-filterSelection(clickedBtn);
-
-for (let i = 0; i < btns.length; i++) {
-
-    btns[i].addEventListener("click", function () {
-
-        current[0].classList.remove("active");
-        this.classList.add("active");
-
-        if (i === 0) {
-            clickedBtn = "all";
-            filterSelection(clickedBtn);
-        } else if (i === 1) {
-            clickedBtn = "lille";
-            filterSelection(clickedBtn);
-        } else if (i === 2) {
-            clickedBtn = "arras";
-            filterSelection(clickedBtn);
-        } else if (i === 3) {
-            clickedBtn = "touquet";
-            filterSelection(clickedBtn);
-        }
-    });
-}
-
-// Fonction de sélection du filtre
-function filterSelection(c) {
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], show);
-        if (x[i].className.indexOf(c) > -1) addClass(x[i], show);
-    }
-}
-// Fonction d'ajout de classe
-function addClass(element) {
-    if (element.className.indexOf(show) == -1) {
-        element.classList.add(show);
-    }
-}
-// Fonction de retrait de classe
-function removeClass(element) {
-    if (element.className.indexOf(show) > -1) {
-        element.classList.remove(show);
-    }
-}
